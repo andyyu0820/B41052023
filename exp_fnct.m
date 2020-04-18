d7414297fada7f1478a99e4438dd9ab47f407fb5
andyyu0820 <63888733+andyyu0820@users.noreply.github.com>
2020-04-18 10:58:54 +0800

    Add files via upload

---
+++ exp_fnct.m
@@ -0,0 +1,11 @@
+%余鎮宏
+%B41052023
+
+function y=exp_fnct(x,funfcn,k,T,p1,p2,p3)
+j=sqrt(-1);
+args=[];
+for nn=1:nargin-4
+    args=[args,',p',int2str(nn)];
+end
+args=[args,')'];
+y=eval([funfcn,'(x',args,'.*exp(-j*2*pi*x*k/T)']);
---
+++ fftseq.m
@@ -0,0 +1,16 @@
+%余鎮宏
+%B41052023
+
+function[M,m,df]=fftseq(m,ts,df)
+
+fs=1/ts;
+if nargin==2
+    n1=0;
+else
+    n1=fs/df;
+end
+n2=length(m);
+n=2^(max(nextpow2(n1),nextpow2(n2)));
+M=fft(m,n);
+m=[m,zeros(1,n-n2)];
+df=fs/n;
